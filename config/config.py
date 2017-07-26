0bdf3098f37cacf29acb7c98996b4e82d342babb
Simon David HERNANDEZ <sdavidhdez.github@totum.one>
2017-07-26 15:07:03 +0200

    Basic configuration. Paths to default corpus.

---
+++ config/config.py
@@ -0,0 +1,11 @@
+corpus = {
+        "acl-rd-tec-2.0": {
+            "path": "corpus/acl-rd-tec-2.0/distribution/",
+            "options": {}
+            },
+        "semeval2017-task10": {
+            "path": "corpus/semeval2017-task10/"
+            }
+        }
+
+corpus["default"] = corpus["semeval2017-task10"]
