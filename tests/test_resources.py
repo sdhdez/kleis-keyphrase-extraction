f00ad84e636def2316c78ace456d6ce45db6f50e
Simon David HERNANDEZ <sdavidhdez.github@totum.one>
2017-07-27 10:58:21 +0200

    Test for the method to get files by extension.

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -2,6 +2,7 @@ from nose2.tools import such
 from nose2.tools.params import params
 
 from resources.items import get_files
+from resources.items import get_files_by_ext
 
 with such.A("module to load resources") as it:
     @it.has_setup
@@ -15,7 +16,6 @@ with such.A("module to load resources") as it:
     @it.should("get files")
     @params(".", "..")
     def test_get_files_basic(case, path):
-        print(path)
         i = 0
         for f in get_files(path):
             i += 1
@@ -26,11 +26,31 @@ with such.A("module to load resources") as it:
     @it.should("not get files")
     @params("why-this-work?", "I'm not a path.", "some_non_existing_path")
     def test_get_files_non_existing(case, path):
-        print(path)
         i = 0
         for f in get_files(path):
             i += 1
             case.assertNotEqual(type(f), str)
+        case.assertEqual(i, 0)
+
+    @it.should("get files by extension")
+    @params((".", "py"), 
+            (".", "PY"), 
+            ("..", "txt"))
+    def test_get_files_by_ext(case, path, ext):
+        i = 0
+        for f in get_files_by_ext(path, ext = ext):
+            i += 1
+        case.assertTrue(i > 0)
+
+    @it.should("not match an extension")
+    @params((".", "pbc"), 
+            (".", "SDF"), 
+            ("..", "I'm not an extension."))
+    def test_get_files_by_ext_not(case, path, ext):
+        i = 0
+        for f in get_files_by_ext(path, ext = ext):
+            i += 1
+            break
         case.assertEqual(i, 0)
 
 it.createTests(globals())
