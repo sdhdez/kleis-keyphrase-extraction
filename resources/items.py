42f746d6dbcf7606c9e4453e178e776d7f5ec16a
Simon David HERNANDEZ <sdavidhdez.github@totum.one>
2017-07-27 10:57:32 +0200

    Method to get files by extension.

--- resources/items.py
+++ resources/items.py
@@ -1,6 +1,22 @@
-import os
+"""resources/items 
 
-def get_files(path_to_files, match = None, exclude = None):
+Module to load corpus
+
+"""
+import os, re
+from config.config import *
+
+def get_files(path_to_files):
     for dirpath, dirnames, filenames in os.walk(path_to_files):
         for fname in filenames:
             yield dirpath + " " + fname
+
+def get_files_by_ext(path_to_files, ext = "txt"):
+    match_ext = re.compile(r"\." + ext + "$", re.IGNORECASE)
+    if len(ext) > 0:
+        for fname in get_files(path_to_files):
+            if match_ext.search(fname):
+                yield fname
+
+def load_corpus_files(corpus = CORPUS_DEFAULT):
+    return corpus
