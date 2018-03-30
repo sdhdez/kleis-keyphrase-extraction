c0af18f926b8eabe3d8623aff10f5409aa931403
Simon David HERNANDEZ <simondhp@git.totum.one>
2018-03-30 15:06:35 +0200

    Keyphrase extration and tests.
    Signed-off-by: Simon David HERNANDEZ <simondhp@git.totum.one>

--- resources/dataset.py
+++ resources/dataset.py
@@ -1,39 +1,47 @@
-"""resources/dataset 
+"""resources/dataset
 
 Module to load corpus
 
 """
-import os, re
+import os
+import re
 from pathlib import Path
-from config.config import *
+from config import config as cfg
 
 def get_files(path_to_files):
-    for dirpath, dirnames, filenames in os.walk(path_to_files):
+    """Walk in path"""
+    for dirpath, _, filenames in os.walk(path_to_files):
         for fname in filenames:
             yield dirpath + "/" + fname
 
-def get_files_by_ext(path_to_files, ext = "txt"):
+def get_files_by_ext(path_to_files, ext="txt"):
+    """Return files by extension"""
     match_ext = re.compile(r"\." + ext + "$", re.IGNORECASE)
-    if len(ext) > 0:
+    if ext:
         for fname in get_files(path_to_files):
             if match_ext.search(fname):
                 yield fname
 
-def is_path_here(path_to_corpus):
-    if path_to_corpus and type(path_to_corpus) == str:
-        return os.path.exists(Path(path_to_corpus)) 
-    else: 
-        return False
+def path_exists(path_to_corpus):
+    """Return true if the path exists, false otherwise"""
+    it_exists = False
+    if path_to_corpus and isinstance(path_to_corpus, str):
+        it_exists = os.path.exists(Path(path_to_corpus))
+    return it_exists
 
 def get_dataset_paths(dataset):
+    """Return dictionary with existing paths"""
     valid_paths = {}
     for name in dataset:
-        if is_path_here(dataset[name]):
+        if path_exists(dataset[name]):
             valid_paths[name] = dataset[name]
     return valid_paths
 
-def load_dataset_semeval2017task10(corpus = CORPUS_SEMEVAL2017_TASK10):
-    if type(corpus) == dict and "dataset" in corpus and type(corpus["dataset"]) == dict:
-        return get_dataset_paths(corpus["dataset"])
-    else:
-        return None
+def load_dataset_semeval2017task10(corpus=None):
+    """Return existing paths of the dataset"""
+    corpus = corpus if corpus is not None else cfg.CORPUS_SEMEVAL2017_TASK10
+    dataset_paths = None
+    if isinstance(corpus, dict) and "dataset" in corpus \
+            and isinstance(corpus["dataset"], dict):
+        dataset_paths = get_dataset_paths(corpus["dataset"])
+    return dataset_paths
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,82 +1,86 @@
+"""Testing module (nose2)"""
+import nose2
 from nose2.tools import such
 from nose2.tools.params import params
 
-from resources import dataset as resources_dataset
+from resources import dataset as rd
 
 with such.A("module to load resources") as it:
     @it.has_setup
     def setup():
+        """Setup"""
         pass
 
     @it.has_teardown
     def teardown():
+        """Teardown"""
         pass
 
     @it.should("get files in path")
     @params(".", "..")
     def test_get_files_basic(case, path):
+        """Test to get files in path"""
         i = 0
-        for fname in resources_dataset.get_files(path):
+        for fname in rd.get_files(path):
             i += 1
             case.assertEqual(type(fname), str)
-        else:
-            case.assertGreater(i, 0)
 
     @it.should("not get files")
     @params("why-this-work?", "I'm not a path.", "some_non_existing_path")
     def test_get_files_non_existing(case, path):
+        """Test wrong finle names"""
         i = 0
-        for fname in resources_dataset.get_files(path):
+        for fname in rd.get_files(path):
             i += 1
             case.assertNotEqual(type(fname), str)
-        else:
-            case.assertEqual(i, 0)
 
     @it.should("get files by extension")
-    @params((".", "py", ".py", 3), 
-            (".", "PY", ".py", 3), 
+    @params((".", "py", ".py", 3),
+            (".", "PY", ".py", 3),
             ("..", "txt", ".txt", 4))
     def test_get_files_by_ext(case, path, ext, match_ext, len_ext):
+        """Test getting files by extension"""
         i = 0
-        for fname in resources_dataset.get_files_by_ext(path, ext = ext):
+        for fname in rd.get_files_by_ext(path, ext=ext):
             i += 1
             case.assertEqual(type(fname), str)
             case.assertEqual(fname[-len_ext:].lower(), match_ext)
-        else:
-            case.assertGreater(i, 0)
 
     @it.should("not match an extension")
-    @params((".", "pbc"), 
-            (".", "SDF"), 
+    @params((".", "pbc"),
+            (".", "SDF"),
             ("..", "I'm not an extension."))
     def test_get_files_by_ext_not(case, path, ext):
+        """Test behavior of trying to get wrong files"""
         i = 0
-        for fname in resources_dataset.get_files_by_ext(path, ext = ext):
+        for fname in rd.get_files_by_ext(path, ext=ext):
             i += 1
             case.assertNotEqual(type(fname), str)
-        else:
-            case.assertEqual(i, 0)
 
     @it.should("check if given directory paths do exist")
     @params(".", "..", "config/", "resources", "./tests/", "./corpus")
-    def test_is_path_here(case, path):
-        case.assertTrue(resources_dataset.is_path_here(path))
+    def test_path_exists(case, path):
+        """Test if given directory paths do exist"""
+        case.assertTrue(rd.path_exists(path))
 
     @it.should("check if given directory paths do NOT exist")
     @params("df sdf sdf s dfs", "abcd dd", " s s d", True, 1, 0, "...", "")
-    def test_is_path_here_not(case, path):
-        case.assertFalse(resources_dataset.is_path_here(path))
+    def test_path_exists_not(case, path):
+        """Check if given directory paths do NOT exist"""
+        case.assertFalse(rd.path_exists(path))
 
     @it.should("return valid paths in dataset")
     @params(({"test0": ".", "test1": ".."}, 2),
-                ({"test0": ".", "test1": "..", "test2": "../", "test3": None}, 3),
-                ({"test0": 1, "test1": "QSDQSD", "test2": "", "test3": None}, 0))
+            ({"test0": ".", "test1": "..", "test2": "../", "test3": None}, 3),
+            ({"test0": 1, "test1": "QSDQSD", "test2": "", "test3": None}, 0))
     def test_get_dataset_paths(case, dataset, n_paths):
-        case.assertEqual(len(resources_dataset.get_dataset_paths(dataset)), n_paths)
+        """Test returning valid paths in dataset"""
+        case.assertEqual(len(rd.get_dataset_paths(dataset)), n_paths)
 
     @it.should("load paths from SemEval2107Task10")
-    def test_load_dataset_semeval2017task10():
-        semeval2017 = resources_dataset.load_dataset_semeval2017task10()
+    def test_load_semeval2017task10():
+        """Test loading paths from SemEval2107Task10"""
+        semeval2017 = rd.load_dataset_semeval2017task10()
         it.assertEqual(len(semeval2017), 4)
         it.assertTrue("train-labeled" in semeval2017)
         it.assertFalse("train-unlabeled" in semeval2017)
@@ -85,13 +89,13 @@ with such.A("module to load resources") as it:
         it.assertTrue("test-labeled" in semeval2017)
         it.assertTrue("test-unlabeled" in semeval2017)
 
-    @it.should("try to load a false dataset")
-    @params({}, None, {"dataset": None}, {"dataset": "."}, 5, [], [1, 3])
-    def test_load_dataset_semeval2017task10(case, dataset):
-        case.assertEqual(resources_dataset.load_dataset_semeval2017task10(corpus=dataset), None)
+    @it.should("fail to load a wrong dataset config")
+    @params({}, {"dataset": None}, {"dataset": "."}, 5, [], [1, 3])
+    def test_wrong_semeval2017task10(case, dataset):
+        """Test try to load a false dataset"""
+        case.assertEqual(rd.load_dataset_semeval2017task10(corpus=dataset), None)
 
 it.createTests(globals())
 
 if __name__ == "__main__":
-    import nose2
     nose2.main()
