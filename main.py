9c4f03388323e21a023ca77ecab545ae65b9452a
Simon David HERNANDEZ <simondhp@git.totum.one>
2018-04-27 15:12:38 +0200

    main.py to test extraction
    Signed-off-by: Simon David HERNANDEZ <simondhp@git.totum.one>

--- main.py
+++ main.py
@@ -8,15 +8,31 @@ import resources.dataset as rd
 def main():
     """Method to run package."""
     # Load default dataset
-    default_dataset = rd.load_corpus()
-    print("Name:", default_dataset.name)
-    print("Config:", default_dataset.config)
-    print("len(Train):", len(default_dataset.train))
-    # print("Train:", default_dataset.train.popitem())
-    print("len(Dev):", len(default_dataset.dev))
-    # print("Dev:", default_dataset.dev.popitem())
-    print("len(Test):", len(default_dataset.test))
-    # print("Test:", default_dataset.test.popitem())
+    default_corpus = rd.load_corpus()
+    print("Name:", default_corpus.name)
+    print("Config:", default_corpus.config)
+    print("len(Train):", len(default_corpus.train))
+    # print("Train:", default_corpus.train.popitem())
+    print("len(Dev):", len(default_corpus.dev))
+    # print("Dev:", default_corpus.dev.popitem())
+    print("len(Test):", len(default_corpus.test))
+    # print("Test:", default_corpus.test.popitem())
+
+    key = list(default_corpus.test.keys())[1]
+    text = default_corpus.test[key]["raw"]["txt"]
+
+    print("Document example...\n")
+    print("Name of document:", key)
+    print("Content to label:\n\n", text)
+
+    # Train or load model
+    default_corpus.training(filter_min_count=3)
+
+    # Labeling
+    keyphrases = default_corpus.label_text(text)
+    print("Example of labeled keyphrases:\n\n", keyphrases)
+    print("\nKeyphrases in %s.ann:\n" % key)
+    print(rd.keyphrases2brat(keyphrases))
 
 if __name__ == "__main__":
     main()
