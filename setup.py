7f9d9c5e319c75775d782aad1f37080c47e97be4
Simon D. Hernandez <sdhdez@totum.one>
2020-04-08 14:56:02 +0200

    Rename version
    Signed-off-by: Simon D. Hernandez <sdhdez@totum.one>

--- setup.py
+++ setup.py
@@ -11,23 +11,21 @@ with open('README.md', 'r', encoding="utf-8") as f:
     README = f.read()
 
 setup(name='kleis-keyphrase-extraction',
-      version='v0.1.3.dev0',
+      version='v0.2a1.dev3',
       description='Python package for keyphrase labeling.',
       long_description=README,
       long_description_content_type='text/markdown',
       url='https://github.com/sdhdez/kleis-keyphrase-extraction',
       author='Simon D. Hernandez',
-      author_email='py.kleis@totum.one',
+      author_email='py.kleis@sdhdez.dev',
       license='MIT',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6'
+          'License :: OSI Approved :: MIT License'
       ],
-      keywords='keyphrase-extraction keyphrase-labeling keyword-extraction crf pos-tag-sequences',
+      keywords='automatic-keyphrase-extraction keyphrase-extraction keyphrase-labeling keyword-extraction crf pos-tag-sequences',
       packages=find_packages('src'),
       package_dir={'':'src', 'kleis': 'src/kleis'},
       package_data={'kleis': ['kleis_data/models/*', 'kleis_data/train/*']},
